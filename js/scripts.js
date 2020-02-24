ab2a0a5a4e281e62bea494b279be435bf87327ad
Kidrash <markkaranja11@gmail.com>
2020-02-24 12:42:55 +0300

    java script modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
-    $("img").click(function(){
-      $("p.design").fadeToggle(500)
-    });
-    $("p.design").click(function() {
-        $("img").fadeToggle(500){
-    });
+  $(".design").click(function(){
+    $(".designp").hide();
+    $(".designp").toggle();
+    $(".designp").show();
+})
+$(document).ready(function() {
+  $(".designp").click(function(){
+    $(".design").hide();
+    $(".designp").toggle();
+    $(".design").show();
+})
+$(document).ready(function() {
+  $(".dev").click(function(){
+    $(".devp").hide();
+    $(".devp").toggle();
+    $(".devp").show();
+})
+$(document).ready(function() {
+  $(".devp").click(function(){
+    $(".dev").hide();
+    $(".devp").toggle();
+    $(".dev").show();
+})
+$(document).ready(function() {
+  $(".prod").click(function(){
+    $(".designp").hide();
+    $(".designp").toggle();
+    $(".designp").show();
+})
+$(document).ready(function() {
+  $(".prodp").click(function(){
+    $(".prod").hide();
+    $(".prodp").toggle();
+    $(".prod").show();
+  });
+
+});

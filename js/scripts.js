c6ad2f99d3b199fcd89e4594b18acee83ad090fd
Kidrash <markkaranja11@gmail.com>
2020-02-24 17:12:39 +0300

    index modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,56 @@
-$(document).ready(function() {
+$(document).ready(function(){
   $(".design").click(function(){
-    $(".designp").hide();
-    $(".designp").toggle();
-    $(".designp").show();
-})
-$(document).ready(function() {
-  $(".designp").click(function(){
     $(".design").hide();
     $(".designp").toggle();
+    $(".designp").show();
+  })
+
+  $(".designp").click(function(){
+    $(".design").toggle();
+    $(".designp").hide();
     $(".design").show();
-})
-$(document).ready(function() {
+  })
+
   $(".dev").click(function(){
-    $(".devp").hide();
-    $(".devp").toggle();
-    $(".devp").show();
-})
-$(document).ready(function() {
-  $(".devp").click(function(){
     $(".dev").hide();
     $(".devp").toggle();
+    $(".devp").show();
+  })
+
+  $(".devp").click(function(){
+    $(".dev").toggle();
+    $(".devp").hide();
     $(".dev").show();
-})
-$(document).ready(function() {
+  })
+
   $(".prod").click(function(){
-    $(".designp").hide();
-    $(".designp").toggle();
-    $(".designp").show();
-})
-$(document).ready(function() {
-  $(".prodp").click(function(){
     $(".prod").hide();
     $(".prodp").toggle();
+    $(".prodp").show();
+  })
+
+  $(".prodp").click(function(){
+    $(".prod").toggle();
+    $(".prodp").hide();
     $(".prod").show();
   });
 
+  $('.hov').hover(function () {
+    $(this).animate({opacity:'0.3'});
+    },
+    function () {
+    $(this).animate({opacity:'0'});
+});
+
+$("button").click(function(event){
+  event.preventDefault();
+  var input1 = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var user = document.getElementById('contactform').reset();
+  if (input1 && email != ''){
+      alert('Thanks ' +   input1 + " we've got you covered.");
+  }else {
+    alert ('Invalid input')
+  }
+   });
 });
